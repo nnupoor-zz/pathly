e40e711c67955d6e69ef43b095385cbfa440140e
nnupoor <nupoor.neha@gmail.com>
2018-04-02 14:23:21 +0530

    warning about errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const getVal =  (path, object)  => {
   if (!Array.isArray(path) || path.length===0) {
-    return new Error('The path entered is incorrect');
+    console.warn('The path entered is incorrect');
+    return null;
   }
   return ( object === undefined )
   ?  (object) => path.reduce((obj, key) => (obj && obj[key]) ? obj[key] : null, object) 
